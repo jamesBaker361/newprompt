3ff650383cbfb4b29e12565e36cb0e7aee1b08a5
James Baker <jlbaker361@gmail.com>
2024-09-04 22:27:26 -0400

    use dot product not cosien sim

--- nearest_neighbors.py
+++ nearest_neighbors.py
@@ -3,6 +3,7 @@ from experiment_helpers.measuring import cos_sim
 from typing import Union
 import numpy as np
 from PIL import Image
+from scipy.linalg import norm
 
 def nearest(ft_src:Union[torch.Tensor, np.ndarray,Image.Image],ft_target:Union[torch.Tensor, np.ndarray,Image.Image],x:int,y:int)->list:
     '''
@@ -14,12 +15,15 @@ def nearest(ft_src:Union[torch.Tensor, np.ndarray,Image.Image],ft_target:Union[t
     '''
     if type(ft_src)==Image.Image:
         ft_src=np.array(ft_src)
+        ft_src = np.transpose(ft_src, (2, 1, 0))
     if type(ft_target)==Image.Image:
         ft_target=np.array(ft_target)
+        ft_target = np.transpose(ft_target, (2, 1, 0))
     src_feature_vector=ft_src[:,x,y]
     if type(ft_target)==torch.tensor:
         (C,W,H)=ft_target.size()
     elif type(ft_target)==np.ndarray:
+        
         (C,W,H)=ft_target.shape
     max_sim=-99999
     max_x=0
@@ -27,14 +31,15 @@ def nearest(ft_src:Union[torch.Tensor, np.ndarray,Image.Image],ft_target:Union[t
     for i in range(W):
         for j in range(H):
             target_feature_vector=ft_target[:,i,j]
-            sim=cos_sim(target_feature_vector, src_feature_vector)
+            sim=np.dot(target_feature_vector, src_feature_vector)
             if sim>max_sim:
-                sim=max_sim
+                max_sim=sim
                 max_x=i
                 max_y=j
     return [[max_x,max_y], max_sim]
 
 if __name__=='__main__':
+    print("starting")
     # Create black 4x4 images
     image1 = Image.new('RGB', (4, 4), color='black')
     image2 = Image.new('RGB', (4, 4), color='black')
@@ -48,4 +53,7 @@ if __name__=='__main__':
 
     # Set red dot in the bottom-right corner for image2
     pixels2[3, 3] = (255, 0, 0)
+
+    print(nearest(image1,image2,0,0))
+    print("ending!?!?")
 
